8be77d88dc11ff748518bc778f955ae113afe0d6
Brian Standage <bstandag@umail.iu.edu>
2018-08-23 17:35:10 -0400

    Cleaned output, allowed for punctuation and capital letters to be inputted.

--- Decoder.py
+++ Decoder.py
@@ -1,20 +1,21 @@
 import enchant
 
-'''Disclaimer: Still in a rough draft state. Will be condensed and modified greatly
-               Punctuation not yet accepted.'''
-
-
-'''function decode: This function accepts a single word and returns the key used
+'''function key_finder: This function accepts a single word and returns the key used
    to decrypt the word.'''
-def decode(word):
+def key_finder(word):
 
     alphabet = "abcdefghijklmnopqrstuvwxyz"
+    upper_alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     dict = enchant.Dict("en_US")
 
     for i in range(1, 27):
         new_word = ''
         for c in word:
-            new_word += alphabet[(alphabet.index(c) + i) % len(alphabet)]
+            if c.isalpha():
+                if c.isupper():
+                    new_word += upper_alpha[(upper_alpha.index(c) + i) % len(upper_alpha)]
+                else:
+                    new_word += alphabet[(alphabet.index(c) + i) % len(alphabet)]
         if dict.check(new_word):
             return i
             break
@@ -26,31 +27,34 @@ def decode(word):
 def decode_key(word, key):
     new_word = ''
     alphabet = "abcdefghijklmnopqrstuvwxyz"
+    upper_alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
     for c in word:
-        new_word += alphabet[(alphabet.index(c) + key) % len(alphabet)]
+        if c.isalpha():
+            if c.isupper():
+                new_word += upper_alpha[(upper_alpha.index(c) + key) % len(upper_alpha)]
+            else:
+                new_word += alphabet[(alphabet.index(c) + key) % len(alphabet)]
+        else:
+            new_word += c
     return new_word
 
-'''function decode_message: This function breaks a message apart into words that can be
-   passed to the decode functions'''
+'''function decode_message: This function breaks a message apart into a list of 
+   words that can be passed to the decode functions, then prints the decoded message'''
 def decode_message(message):
-
-    alphabet = "abcdefghijklmnopqrstuvwxyz"
-    dict = enchant.Dict("en_US")
     words = message.split()
     decoded_words = []
     keys = []
 
-
-    
     for w in words:
-        keys.append(decode(w))
+        keys.append(key_finder(w))
 
     for w in words:
+        # here the mode of the keys is passes to decode_key.
+        # this prevents unintentional words from being printed
         decoded_words.append(decode_key(w, max(set(keys), key=keys.count)))
 
-    print(words)
-    print(decoded_words)
+    print(str.join(' ', decoded_words))
 
 
 if __name__ == '__main__':
